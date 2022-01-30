e4f24e91ffa9e9c7982bdeccec43e5eb1404bb78
Andre Manchanthasouk <dremanchan@gmail.com>
2022-01-29 23:12:16 -0600

    fixed log.

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ function* fetchDetails(action) {
     try {
         const details = yield axios.get('/api/details', {params: {id: action.payload}});
         console.log('GET details results', details.data);
-        yield put({ type: 'SET_DETAILS', payload: detailPage.data });
+        yield put({ type: 'SET_DETAILS', payload: details.data });
     }
     catch (err) {
         console.error('fetchDetails failed', err);
